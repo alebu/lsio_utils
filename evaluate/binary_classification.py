84f4b414ac4590f7c79d217d373ccc6c96c13675
alebu <alessio.burani@gmail.com>
2017-10-15 21:22:07 +0100

    renamed gini metric function

--- evaluate/binary_classification.py
+++ evaluate/binary_classification.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import roc_curve, auc
 
 
-def calculate_gini(y_true, y_score):
+def gini(y_true, y_score):
     fpr, tpr, _ = roc_curve(y_true, y_score)
     return 2*auc(fpr, tpr) - 1
